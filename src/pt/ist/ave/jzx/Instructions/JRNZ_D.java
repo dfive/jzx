fdf23813a404e8918b89789fecd940f01363fcc3
mbborgez <mb.borgez@gmail.com>
2013-04-14 19:12:33 +0100

    optimizing one of the most used instructions - JRNZ_D

--- src/pt/ist/ave/jzx/Instructions/JRNZ_D.java
+++ src/pt/ist/ave/jzx/Instructions/JRNZ_D.java
@@ -1,9 +1,21 @@
 package pt.ist.ave.jzx.Instructions;
 
+import java.util.HashMap;
+
 
 public class JRNZ_D extends Instruction {
 
 	private int _tstates;
+	
+	private static HashMap<Integer, Integer> addsCache = new HashMap<Integer, Integer>();
+	
+	private int getAdd(){
+		int m_pc16 = _cpu.getM_pc16();
+		if(!addsCache.containsKey(m_pc16)){
+			addsCache.put(m_pc16, _cpu.add16(m_pc16, (byte) readNextMemoryPosition() + 1));
+		}
+		return addsCache.get(m_pc16);
+	}
 	
 	public JRNZ_D(short opCode) {
 		super(opCode);
@@ -13,8 +25,7 @@ public class JRNZ_D extends Instruction {
 	public void execute() {
 		if (!_cpu.getM_zeroF()) {
 			_tstates = 12;
-			int m_pc16 = _cpu.getM_pc16();
-			_cpu.setM_pc16(_cpu.add16(m_pc16, (byte) _cpu.getM_memory().read8(m_pc16) + 1));
+			_cpu.setM_pc16(getAdd());
 		} else {
 			_tstates = 7;
 			_cpu.inc16pc();
